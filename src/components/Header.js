e20951a9335a6bdaa8251a144d4152f38c80ea9a
mariotmc <mariotomicx@gmail.com>
2022-01-08 14:04:33 +0100

    Fix routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,19 +7,19 @@ const Header = () => {
   return (
     <header>
       <nav>
-        <Link id="logo" to="item-shop/">
+        <Link id="logo" to="/item-shop/">
           <img src={logo} alt="Logo" />
         </Link>
         <ul>
-          <Link to="item-shop/">
+          <Link to="/item-shop/">
             <li>Home</li>
           </Link>
 
-          <Link to="item-shop/shop">
+          <Link to="/item-shop/shop">
             <li>Shop</li>
           </Link>
 
-          <Link to="item-shop/contact">
+          <Link to="/item-shop/contact">
             <li>Contact</li>
           </Link>
 
