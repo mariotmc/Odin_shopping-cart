2d16c2cede0ff509843570740ec39a21871f9c01
mariotmc <mariotomicx@gmail.com>
2022-01-08 11:16:54 +0100

    Add header component

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../media/logo.png";
+import cart from "../media/cart.png";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <Link id="logo" to="/">
+          <img src={logo} alt="Logo" />
+        </Link>
+        <ul>
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+
+          <Link to="/shop">
+            <li>Shop</li>
+          </Link>
+
+          <Link to="/contact">
+            <li>Contact</li>
+          </Link>
+
+          <li>
+            <div id="cart-image-container">
+              <img id="cart-image" src={cart} alt="Cart" />
+            </div>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
